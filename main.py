293df8ed7193362099dfc86d7c5b9d7292bc380f
grant <gtweaver4@gmail.com>
2016-11-20 12:05:28 -0500

    added some more helpeful comments and made more readable variable names

--- main.py
+++ main.py
@@ -6,37 +6,36 @@ from PIL import Image
 import os
 import shutil
 
-photo_path = ""
+#created these variables to be global
+folder_path = ""
 remembered_file = 'watermarker_remembered_path.txt'
 
+#watermarks entire folder and creates a directory
+#if directory doesnt already exist
 def watermark_folder():
-    global photo_path
-    files = os.listdir(photo_path)
-    if not os.path.exists(photo_path + "/watermarked_images"):
-        os.mkdir(photo_path + '/watermarked_images')
+    global folder_path
+    files = os.listdir(folder_path)
+    if not os.path.exists(folder_path + "/watermarked_images"):
+        os.mkdir(folder_path + '/watermarked_images')
     for f in files:
-        watermark_file((photo_path + "/" + f))
+        watermark_file((folder_path + "/" + f))
 
     move_files()
     print('completed')
     sys.exit()
 
 
+#gets a directory to set as the folder_path
 def ask_directory():
-    #returns a selected directory
-    global photo_path 
-    photo_path = askdirectory()
+    global folder_path 
+    folder_path = askdirectory()
 
 #creates the file selector GUI
 def file_selector():
     return askopenfilename()
 
-#setting photo photo_path
-def set_photo_path():
-    global photo_path
-    photo_path = file_selector()
-
 #imports and saves watermark location
+#into txt file
 def import_watermark_path():
     #since not using append it clears file automatically
     f = open(remembered_file, 'w')
@@ -47,21 +46,20 @@ def import_watermark_path():
     else:
         print('Please select a .JPG, .JPEG, or .PNG image')
 
+#moves files from the source directory to the location of the photo imports
+#this relies on the watermarked_images folder to have been created
 def move_files():
-    global photo_path
+    global folder_path
     source_folder = os.getcwd()
-    destination_folder = photo_path + "/watermarked_images"
-    #if not os.path.exists(destination_folder):
-        #os.mkdir(destination_folder, 0755)
-
+    destination_folder = folder_path + "/watermarked_images"
     files = os.listdir(source_folder)
-
     for f in files:
         if(f.endswith('.jpg') or f.endswith('jpeg') or f.endswith('png')):
             full_path = source_folder + "/" + f
             shutil.move(full_path , destination_folder)
 
 
+#this watermarks a singular image
 def watermark_file(file_path):
     watermark_path = open(remembered_file, 'r').read()
     watermark_image = Image.open(watermark_path)
@@ -72,7 +70,7 @@ def watermark_file(file_path):
     split = file_path.split('/')
     queued_image.save(split[-1])
 
-#main tk window
+#main tk window for simple GUI
 root = Tk()
 root.title('Watermarker')
 root.geometry('360x200')
@@ -82,7 +80,7 @@ import_button = Button(root, text = "Import", command = lambda root = root:ask_d
 
 watermarker_label = Label(root, text = "Select your watermark").grid(row = 2)
 select_button = Button(root, text = "Select", command = lambda root = root:import_watermark_path()).grid(row = 3)
-reminder_label = Label(root, text = "Reminder: Your last used watermark will be saved for reuse").grid(row = 4)
+watermark_image = PhotoImage(file = open(remembered_file, 'r').read())
 watermarker = Button(root, text = 'Process', command = lambda root = root: watermark_folder()).grid(row = 5)
 
 cancel_button = Button(root, text = "Cancel", command = lambda root = root:sys.exit()).grid(row = 6)
