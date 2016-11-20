5bb35759e7345e1ae80028520342142a79362939
grant <gtweaver4@gmail.com>
2016-11-19 20:39:49 -0500

    updated the program so it can watermark entire folders

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from Tkinter import *
 from tkFileDialog import askopenfilename
+from tkFileDialog import askdirectory
 import sys
 from PIL import Image
+import os
 
 photo_path = ""
 remembered_file = 'watermarker_remembered_path.txt'
+
+def watermark_folder():
+    global photo_path
+    files = os.listdir(photo_path)
+    for f in files:
+        watermark_file((photo_path + "/" + f))
+
+
+
+def ask_directory():
+    #returns a selected directory
+    global photo_path 
+    photo_path = askdirectory()
 
 #creates the file selector GUI
 def file_selector():
@@ -27,24 +42,28 @@ def import_watermark_path():
         print('Please select a .JPG, .JPEG, or .PNG image')
 
 
-def watermark_files():
+def watermark_file(file_path):
     watermark_path = open(remembered_file, 'r').read()
     watermark_image = Image.open(watermark_path)
-    queued_image = Image.open(photo_path)
-    print('do all the things')
+    watermark_width, watermark_height = watermark_image.size
+    queued_image = Image.open(file_path)
+    queue_width, queue_height = queued_image.size
+    queued_image.paste(watermark_image, (queue_width-watermark_width, queue_height-watermark_height))
+    split = file_path.split('/')
+    queued_image.save(split[-1])
 
 #main tk window
 root = Tk()
 root.title('Watermarker')
 root.geometry('360x200')
 
-select_label = Label(root, text = "Please import the file you wish to watermark").grid(row = 0)
-import_button = Button(root, text = "Import", command = lambda root = root:set_photo_path()).grid(row = 1)
+select_label = Label(root, text = "Please import the folder you wish to watermark").grid(row = 0)
+import_button = Button(root, text = "Import", command = lambda root = root:ask_directory()).grid(row = 1)
 
 watermarker_label = Label(root, text = "Select your watermark").grid(row = 2)
 select_button = Button(root, text = "Select", command = lambda root = root:import_watermark_path()).grid(row = 3)
 reminder_label = Label(root, text = "Reminder: Your last used watermark will be saved for reuse").grid(row = 4)
-watermarker = Button(root, text = 'Watermark', command = lambda root = root: watermark_files()).grid(row = 5)
+watermarker = Button(root, text = 'Process', command = lambda root = root: watermark_folder()).grid(row = 5)
 
 cancel_button = Button(root, text = "Cancel", command = lambda root = root:sys.exit()).grid(row = 6)
 
